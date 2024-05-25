397d3301df07e7971b3ff94cd368bb12e593fab7
Mehmet Hayri Çakır <36895137+mhmtckr27@users.noreply.github.com>
2024-05-25 11:55:33 +0300

    opened some comments

--- Packages/com.hayricakir.playerdata/Presentation/PlayerDataPresentationInstaller.cs
+++ Packages/com.hayricakir.playerdata/Presentation/PlayerDataPresentationInstaller.cs
@@ -1,12 +1,12 @@
-﻿// using Zenject;
-//
-// namespace PlayerData.Presentation
-// {
-//     public class PlayerDataPresentationInstaller : MonoInstaller
-//     {
-//         public override void InstallBindings()
-//         {
-//             Container.Bind<PlayerDataController>().AsSingle().NonLazy();
-//         }
-//     }
-// }
+﻿using Zenject;
+
+namespace PlayerData.Presentation
+{
+    public class PlayerDataPresentationInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<PlayerDataController>().AsSingle().NonLazy();
+        }
+    }
+}
